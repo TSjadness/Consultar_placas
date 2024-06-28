7b746c64ac8489a0bb020be3daa7413691dc9991
TSjadness <jadsontavarests@hotmail.com>
2024-06-28 17:35:27 -0300

    creat form

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import imagemFundo from '@/src/images/car-bg.jpg'
-import logocar from "@/src/images/logo-car.png"
+import { useForm } from "react-hook-form";
+import imagemFundo from "@/src/images/car-bg.jpg";
+import logocar from "@/src/images/logo-car.png";
 import Image from "next/image";
 
 const HomePage = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+  } = useForm();
+
+  const onSubmit = async (data: any) => {
+    console.log(data);
+  };
+
   return (
     <>
       <link
@@ -24,15 +35,6 @@ const HomePage = () => {
               className="w-full h-full absolute opacity-75 bg-black"
             ></span>
           </div>
-
-          {/* <div
-            className="absolute top-0 w-full h-full bg-center bg-cover"
-            style={{
-              backgroundImage: `url(${imagemFundo})`
-            }}
-          >
-            
-          </div> */}
           <div className="container relative mx-auto">
             <div className="items-center flex flex-wrap">
               <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
@@ -48,72 +50,49 @@ const HomePage = () => {
             </div>
           </div>
 
-          <div className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px">
-            {/* <svg
-              className="absolute bottom-0 overflow-hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="text-blueGray-200 fill-current"
-                points="2560 0 2560 100 0 100"
-              ></polygon>
-            </svg> */}
-          </div>
+          <div className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"></div>
         </div>
         <section className="pb-10 bg-blueGray-200 -mt-24">
           <div className="container mx-auto px-4">
             <div className="bg-red-600 flex justify-center items-center">
-              {/* <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
-                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                  <div className="px-4 py-5 flex-auto">
-                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
-                      <i className="fas fa-award"></i>
-                    </div>
-                    <h6 className="text-xl font-semibold">Awarded Agency</h6>
-                    <p className="mt-2 mb-4 text-blueGray-500">
-                      Divide details about your product or agency work into
-                      parts. A paragraph describing a feature will be enough.
-                    </p>
-                  </div>
-                </div>
-              </div> */}
-
-              {/* <div className="w-full md:w-4/12 px-4 text-center">
-                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                  <div className="px-4 py-5 flex-auto">
-                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
-                      <i className="fas fa-retweet"></i>
-                    </div>
-                    <h6 className="text-xl font-semibold">Free Revisions</h6>
-                    <p className="mt-2 mb-4 text-blueGray-500">
-                      Keep you user engaged by providing meaningful information.
-                      Remember that by this time, the user is curious.
-                    </p>
-                  </div>
-                </div>
-              </div> */}
-
               <div className="pt-6 w-full md:w-8/12 px-4 text-center">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
                     <div className="text-white p-0 text-center inline-flex items-center justify-center w-12 h-12  shadow-lg rounded-full bg-emerald-400">
                       <Image
                         src={logocar}
-                        // layout="fill"
                         width={200}
                         height={200}
-                        className="rounded-full"
+                        className="rounded-full p-0 text-center inline-flex items-center justify-center w-12 h-12  shadow-lg rounded-full bg-emerald-400"
                         objectFit="cover"
                         alt=""
                       />
-                      {/* <i className="fas fa-fingerprint"></i> */}
                     </div>
-                    <h6 className="text-xl font-semibold">Verified Company</h6>
+                    <form action="" onSubmit={handleSubmit(onSubmit)}  className="bg-yellow-500 w-full">
+                      <h6 className="text-xl font-semibold text-black">
+                        Indique a placa do veículo
+                      </h6>
+                      <div className="w-full bg-black">
+                        <input
+                          id="placa"
+                          className="border rounded-3xl border-gray-300 p-2  mt-2 bg-gray-100 text-black"
+                          type="text"
+                          {...register("placa")}
+                          placeholder="Digite a placa do veículo"
+                        />
+                      </div>
+
+                      <div className="bg-teal-600 w-full">
+                        <button
+                        type="submit"
+                        className="text-black bg-red-500  hover:bg-opacity-80 mt-4 focus:outline-none focus:ring-2"
+                      >
+                        Pesquisar
+                      </button>
+                      </div>
+
+                      
+                    </form>
                     <p className="mt-2 mb-4 text-blueGray-500">
                       Write a few lines about each one. A paragraph describing a
                       feature will be enough. Keep you user engaged!
@@ -123,35 +102,6 @@ const HomePage = () => {
               </div>
             </div>
           </div>
-          {/* <footer className="relative pt-8 pb-6 mt-1">
-            <div className="container mx-auto px-4">
-              <div className="flex flex-wrap items-center md:justify-between justify-center">
-                <div className="w-full md:w-6/12 px-4 mx-auto text-center">
-                  <div className="text-sm text-blueGray-500 font-semibold py-1">
-                    Made with{" "}
-                    <a
-                      href="https://www.creative-tim.com/product/notus-js"
-                      className="text-blueGray-500 hover:text-gray-800"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Notus JS
-                    </a>{" "}
-                    by{" "}
-                    <a
-                      href="https://www.creative-tim.com"
-                      className="text-blueGray-500 hover:text-blueGray-800"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Creative Tim
-                    </a>
-                    .
-                  </div>
-                </div>
-              </div>
-            </div>
-          </footer> */}
         </section>
       </section>
     </>
